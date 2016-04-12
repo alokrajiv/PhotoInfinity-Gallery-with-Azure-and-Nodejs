c8448c03991e0a9d5bdb7f4791fd53c1f588cf94
alokrajiv <mail@alokrajiv.com>
2016-04-12 23:29:26 +0400

    Added favicon. And, before socket inclusions for app

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ var path = require('path');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+
+app.use(favicon(__dirname + '/public/static/favicon.ico'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
